ef1a3763fd6bb047ccafdc7bd372b081b2ff3d31
Leighton Miguel <leim56@hawaii.rr.com>
2024-08-11 15:56:35 -1000

    Devplate test

--- test/devplate.test.ts
+++ test/devplate.test.ts
@@ -1,38 +1,151 @@
-import { describe, expect, test } from "@jest/globals";
-import { Devplate } from '../src/devplate/Devplate'
+import { describe, expect, test, jest, beforeEach } from "@jest/globals";
+import { promises as fs } from "fs";
+import { Devplate } from "../src/devplate/Devplate";
+import * as path from "path";
 
-describe("Devplate class", () => {
-  const devplate = new Devplate();
+jest.mock("fs", () => ({
+  promises: {
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
+  },
+}));
 
-  test("Add not yet existing Devplate repository return 1", async () => {
-    const add = await devplate.addDevplateRepository({
-      name: "New Devplate repo name",
-      url: "New Devplate repo URL",
-    });
-    expect(add).toBe(1);
+jest.mock("path", () => ({
+  join: jest.fn(),
+}));
+
+jest.mock("inquirer", () => ({
+  prompt: jest.fn(),
+}));
+
+describe("Devplate", () => {
+  let devplate: Devplate;
+
+  beforeEach(() => {
+    devplate = new Devplate();
+    jest.clearAllMocks();
   });
 
-  test("Add already existing Devplate repository return 0", async () => {
-    const add = await devplate.addDevplateRepository({
-      name: "New Devplate repo name",
-      url: "New Devplate repo URL",
+  describe("setDevplateRepositories", () => {
+    test("should read and parse devplates.json", async () => {
+      const mockData = JSON.stringify({
+        devplateRepositories: [{ name: "Repo1", url: "https://repo1.com" }],
+      });
+      (fs.readFile as jest.Mock).mockResolvedValue(mockData as never);
+      (path.join as jest.Mock).mockReturnValue("/mock/path/devplates.json");
+
+      const repositories = await devplate["setDevplateRepositories"]();
+
+      expect(fs.readFile).toHaveBeenCalledWith(
+        "/mock/path/devplates.json",
+        "utf-8"
+      );
+      expect(repositories).toEqual([
+        { name: "Repo1", url: "https://repo1.com" },
+      ]);
     });
-    expect(add).toBe(0);
+
+    test("should return an empty array if an error occurs", async () => {
+      (fs.readFile as jest.Mock).mockRejectedValue(
+        new Error("File read error") as never
+      );
+      const repositories = await devplate["setDevplateRepositories"]();
+      expect(repositories).toEqual([]);
+    });
   });
 
-  test("Remove existing Devplate repository return 1", async () => {
-    const remove = await devplate.removeDevplateRepository({
-      name: "New Devplate repo name",
-      url: "New Devplate repo URL",
+  describe("devplateRepositryExists", () => {
+    test("should return true if repository exists", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const exists = await devplate["devplateRepositryExists"]({
+        name: "Repo1",
+        url: "https://repo1.com",
+      });
+
+      expect(exists).toBe(true);
     });
-    expect(remove).toBe(1);
+
+    test("should return false if repository does not exist", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const exists = await devplate["devplateRepositryExists"]({
+        name: "Repo2",
+        url: "https://repo2.com",
+      });
+
+      expect(exists).toBe(false);
+    });
   });
 
-  test("Remove non-existing Devplate repository return 0", async () => {
-    const remove = await devplate.removeDevplateRepository({
-      name: "New Devplate repo name",
-      url: "New Devplate repo URL",
+  describe("updateDevplateRepositoryJson", () => {
+    test("should write new JSON to devplates.json and update repositories", async () => {
+      const newJson = {
+        devplateRepositories: [{ name: "Repo1", url: "https://repo1.com" }],
+      };
+      (path.join as jest.Mock).mockReturnValue("/mock/path/devplates.json");
+
+      await devplate["updateDevplateRepositoryJson"](newJson);
+
+      expect(fs.writeFile).toHaveBeenCalledWith(
+        "/mock/path/devplates.json",
+        JSON.stringify(newJson, null, 2),
+        "utf-8"
+      );
     });
-    expect(remove).toBe(0);
+  });
+
+  describe("addDevplateRepository", () => {
+    test("should add a new repository if it does not exist", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const newRepository = { name: "Repo2", url: "https://repo2.com" };
+      const result = await devplate.addDevplateRepository(newRepository);
+
+      expect(result).toBe(1);
+      expect(fs.writeFile).toHaveBeenCalled();
+    });
+
+    test("should not add a repository if it already exists", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const newRepository = { name: "Repo1", url: "https://repo1.com" };
+      const result = await devplate.addDevplateRepository(newRepository);
+
+      expect(result).toBe(0);
+      expect(fs.writeFile).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("removeDevplateRepository", () => {
+    test("should remove an existing repository", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const repositoryToRemove = { name: "Repo1", url: "https://repo1.com" };
+      const result = await devplate.removeDevplateRepository(
+        repositoryToRemove
+      );
+
+      expect(result).toBe(1);
+      expect(fs.writeFile).toHaveBeenCalled();
+    });
+
+    test("should not remove a repository if it does not exist", async () => {
+      const mockRepositories = [{ name: "Repo1", url: "https://repo1.com" }];
+      devplate["devplateRepositories"] = Promise.resolve(mockRepositories);
+
+      const repositoryToRemove = { name: "Repo2", url: "https://repo2.com" };
+      const result = await devplate.removeDevplateRepository(
+        repositoryToRemove
+      );
+
+      expect(result).toBe(0);
+      expect(fs.writeFile).not.toHaveBeenCalled();
+    });
   });
 });
--- test/puller.test.ts
+++ test/puller.test.ts
@@ -36,78 +36,80 @@ describe("Puller class", () => {
 
   describe("initialize", () => {
     test("should fetch and initialize devplates", async () => {
-        const mockResponse = [
-          { name: "devplate1" },
-          { name: "devplate2" },
-        ];
-  
-        const mockFetchResponse = {
-          ok: true,
-          json: async () => mockResponse,
-        } as unknown as Response;
-  
-        (global.fetch as FetchMock).mockResolvedValue(mockFetchResponse);
-  
-        await puller.initialize();
-  
-        expect(global.fetch).toHaveBeenCalledWith(
-          "https://api.github.com/repos/user/repo/contents"
-        );
-        expect(puller["devplates"]).toEqual(mockResponse);
-      });
+      const mockResponse = [{ name: "devplate1" }, { name: "devplate2" }];
 
-      test("should handle fetch errors", async () => {
-        const mockFetchResponse = {
-          ok: false,
-          json: async () => [],
-        } as unknown as Response;
-      
-        (global.fetch as FetchMock).mockResolvedValue(mockFetchResponse);
-      
-        const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-      
-        await puller.initialize();
-      
-        expect(global.fetch).toHaveBeenCalledWith(
-          "https://api.github.com/repos/user/repo/contents"
-        );
-        expect(consoleSpy).toHaveBeenCalledWith(new Error("Failed to fetch data"));
-        expect(puller["devplates"]).toEqual([]);
-        
-        consoleSpy.mockRestore();
-      });
+      const mockFetchResponse = {
+        ok: true,
+        json: async () => mockResponse,
+      } as unknown as Response;
+
+      (global.fetch as FetchMock).mockResolvedValue(mockFetchResponse);
+
+      await puller.initialize();
+
+      expect(global.fetch).toHaveBeenCalledWith(
+        "https://api.github.com/repos/user/repo/contents"
+      );
+      expect(puller["devplates"]).toEqual(mockResponse);
+    });
+
+    test("should handle fetch errors", async () => {
+      const mockFetchResponse = {
+        ok: false,
+        json: async () => [],
+      } as unknown as Response;
+
+      (global.fetch as FetchMock).mockResolvedValue(mockFetchResponse);
+
+      const consoleSpy = jest
+        .spyOn(console, "log")
+        .mockImplementation(() => {});
+
+      await puller.initialize();
+
+      expect(global.fetch).toHaveBeenCalledWith(
+        "https://api.github.com/repos/user/repo/contents"
+      );
+      expect(consoleSpy).toHaveBeenCalledWith(
+        new Error("Failed to fetch data")
+      );
+      expect(puller["devplates"]).toEqual([]);
+
+      consoleSpy.mockRestore();
+    });
   });
 
   describe("logDevplates", () => {
     test("should log devplate names", async () => {
-        puller["devplates"] = [
-          { name: "devplate1" },
-          { name: "devplate2" },
-        ];
-      
-        const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-      
-        await puller.logDevplates();
-      
-        expect(consoleSpy).toHaveBeenCalledWith(
-          "\nFetching list of Devplate names in repository https://github.com/user/repo\n"
-        );
-        expect(consoleSpy).toHaveBeenCalledWith("1. devplate1");
-        expect(consoleSpy).toHaveBeenCalledWith("2. devplate2");
-      
-        consoleSpy.mockRestore();
-      });
-      
-      test("should not log anything if devplates is undefined", async () => {
-        puller["devplates"] = undefined;
+      puller["devplates"] = [{ name: "devplate1" }, { name: "devplate2" }];
 
-        const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-      
-        await puller.logDevplates();
-      
-        expect(consoleSpy).toHaveBeenCalledTimes(1);
-      
-        consoleSpy.mockRestore();
-      });
+      const consoleSpy = jest
+        .spyOn(console, "log")
+        .mockImplementation(() => {});
+
+      await puller.logDevplates();
+
+      expect(consoleSpy).toHaveBeenCalledWith(
+        "\nFetching list of Devplate names in repository https://github.com/user/repo\n"
+      );
+      expect(consoleSpy).toHaveBeenCalledWith("1. devplate1");
+      expect(consoleSpy).toHaveBeenCalledWith("2. devplate2");
+
+      consoleSpy.mockRestore();
+    });
+
+    test("should not log anything if devplates is undefined", async () => {
+      puller["devplates"] = undefined;
+
+      const consoleSpy = jest
+        .spyOn(console, "log")
+        .mockImplementation(() => {});
+
+      await puller.logDevplates();
+
+      expect(consoleSpy).toHaveBeenCalledTimes(1);
+
+      consoleSpy.mockRestore();
+    });
   });
 });
